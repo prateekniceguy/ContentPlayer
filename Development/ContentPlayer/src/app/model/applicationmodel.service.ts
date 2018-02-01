3d3d2e07c67ec06e5bd4e76a3140926a2cf00180
unknown <prateek.niceguy@gmail.com>
2018-02-01 12:25:59 +0530

    external communication integrated

--- Development/ContentPlayer/src/app/model/applicationmodel.service.ts
+++ Development/ContentPlayer/src/app/model/applicationmodel.service.ts
@@ -4,5 +4,7 @@ import { Injectable } from '@angular/core';
 export class ApplicationmodelService {
 
   constructor() { }
+  init() {
+  }
 
 }
--- Development/ContentPlayer/src/app/model/externalcommunication.service.ts
+++ Development/ContentPlayer/src/app/model/externalcommunication.service.ts
@@ -1,8 +1,66 @@
-import { Injectable } from '@angular/core';
+import {Injectable, EventEmitter} from '@angular/core';
+
+declare var $: any;
 
 @Injectable()
 export class ExternalcommunicationService {
 
-  constructor() { }
+  private proxy: any;
+  private proxyName: String = 'sr';
+  private connection: any;
+  // create the Event Emitter
+  public messageReceived: EventEmitter<any>;
+  public connectionEstablished: EventEmitter<Boolean>;
+  public connectionExists: Boolean;
+
+  constructor() {
+    // Constructor initialization
+    this.connectionEstablished = new EventEmitter<Boolean>();
+    this.messageReceived = new EventEmitter<any>();
+    this.connectionExists = false;
+    // create hub connection
+    this.connection = $.hubConnection('http://localhost:8082/signalr');
+    // create new proxy as name already given in to
+    this.proxy = this.connection.createHubProxy(this.proxyName);
+    // register on server events
+    this.registerOnServerEvents();
+    // call the connecion start method to start the connection to send and receive events.
+    this.startConnection();
+  }
+
+  // method to hit from client
+  public sendTime() {
+    // server side hub method using proxy.invoke with method name pass as param
+    this.proxy.invoke('GetRealTime');
+  }
+  // check in the browser console for either signalr connected or not
+  private startConnection(): void {
+    this.connection.start().done((data: any) => {
+      console.log('Now connected ' + data.transport.name + ', connection ID= ' + data.id);
+      this.connectionEstablished.emit(true);
+      this.connectionExists = true;
+    }).fail((error: any) => {
+      console.log('Could not connect ' + error);
+      this.connectionEstablished.emit(false);
+    });
+  }
+  private registerOnServerEvents(): void {
+    this.proxy.on('open', (data: any) => {
+      console.log('received in SignalRService: ' + JSON.stringify(data));
+      this.messageReceived.emit(data);
+    });
+    this.proxy.on('cmsPlayerClose', (data: any) => {
+      console.log('received in SignalRService: ' + JSON.stringify(data));
+      this.messageReceived.emit(data);
+    });
+    this.proxy.on('cmsPlayerPlay', (data: any) => {
+      console.log('received in SignalRService: ' + JSON.stringify(data));
+      this.messageReceived.emit(data);
+    });
+    this.proxy.on('cmsPlayerPause', (data: any) => {
+      console.log('received in SignalRService: ' + JSON.stringify(data));
+      this.messageReceived.emit(data);
+    });
+  }
 
 }
