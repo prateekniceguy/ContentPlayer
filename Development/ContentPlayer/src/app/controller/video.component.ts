bfa0fc25c4de6b84047b3c65cf3aefd9b29ee690
unknown <prateek.niceguy@gmail.com>
2018-02-23 12:23:18 +0530

    major changes stable version

--- Development/ContentPlayer/src/app/controller/video.component.ts
+++ Development/ContentPlayer/src/app/controller/video.component.ts
@@ -1,19 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
+declare var Slider: any;
 
 @Component({
   selector: 'app-video',
   templateUrl: '../view/layout/video.component.html',
-  styleUrls: ['../view/css/video.component.css']
+  styleUrls: ['../view/css/video.component.css', '../view/css/bootstrap.css',
+     '../view/css/bootstrap-slider.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class VideoComponent implements OnInit {
 
-  someRange = 15;
-
+  someRange = 5;
+  sliderRef = null;
   constructor() {
   }
 
   ngOnInit() {
+    /*this.sliderRef = new Slider('#seek-bar', { id: "slider5a",
+     * formatter: updateTimeTT, min: 0, max: 100, value: 0, forwardValue: maxtime + 1, forwardEnabled: !p });
+     */
+    this.sliderRef = new Slider('#seek-bar', { id: 'slider5a',
+      min: 0, max: 100, value: 0, forwardValue: 50, forwardEnabled: false });
   }
 
 }
