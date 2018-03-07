093c0c113c00caa53175871e1ccad7455666e140
shiksha <shiksha@DESKTOP-4ANLMM9>
2018-03-07 08:12:30 +0530

    wip

--- Development/ContentPlayer/src/app/model/httphandler.service.ts
+++ Development/ContentPlayer/src/app/model/httphandler.service.ts
@@ -1,7 +1,7 @@
 import {HttpErrorResponse} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable} from 'rxjs/Observable';
 import {ErrorObservable} from 'rxjs/observable/ErrorObservable';
 import {catchError, retry} from 'rxjs/operators';
 
