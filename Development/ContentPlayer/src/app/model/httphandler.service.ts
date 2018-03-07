7c7183afdefdb45dd7b29a61d263f94e00fe2029
shiksha <shiksha@DESKTOP-4ANLMM9>
2018-03-07 08:15:37 +0530

    wip

--- Development/ContentPlayer/src/app/model/httphandler.service.ts
+++ Development/ContentPlayer/src/app/model/httphandler.service.ts
@@ -4,6 +4,7 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {ErrorObservable} from 'rxjs/observable/ErrorObservable';
 import {catchError, retry} from 'rxjs/operators';
+import 'rxjs/add/observable/forkJoin';
 
 @Injectable()
 export class HttphandlerService {
